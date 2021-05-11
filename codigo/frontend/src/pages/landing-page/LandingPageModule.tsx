adb479bf2fb1d1dc6befcbde96377f9718c04efe
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2021-05-11 00:47:17 -0300

    FRONT: BugFix gerais, melhorias TBD #20

--- codigo/frontend/src/pages/landing-page/LandingPageModule.tsx
+++ codigo/frontend/src/pages/landing-page/LandingPageModule.tsx
@@ -9,83 +9,83 @@ export default class LandingPageModule extends Component {
     render() {
         return (
             <>
-                <header class="header">
-                    <a class="logo" href="#home"><img src="./img/logo-white.svg" alt="DevTask logo" /></a>
-                    <a class="btn-menu"><i class="fas fa-bars"></i></a>
-                    <nav class="menu">
-                        <ul class="nav-list">
-                            <li><a class="nav-link" href="#devtask">O DevTask</a></li>
-                            <li><a class="nav-link" href="#aboutUs">Sobre Nós</a></li>
-                            <li><a class="nav-link btn btn-register" href="#" role="button">Cadastro</a></li>
-                            <li><a class="nav-link btn btn-login" href="#">Login</a></li>
+                <header className="header">
+                    <a className="logo" href="/"><img src={logo} alt="DevTask logo" /></a>
+                    <a className="btn-menu"><i className="fas fa-bars"></i></a>
+                    <nav className="menu">
+                        <ul className="nav-list">
+                            <li><a className="nav-link" href="#devtask">O DevTask</a></li>
+                            <li><a className="nav-link" href="#aboutUs">Sobre Nós</a></li>
+                            <li><a className="nav-link btn btn-register" href="/register" role="button">Cadastro</a></li>
+                            <li><a className="nav-link btn btn-login" href="/login">Login</a></li>
                         </ul>
                     </nav>
                 </header>
 
-                <section class="bg-primary">
-                    <div class="container" id="home">
-                        <div class="split">
-                            <div class="text">
+                <section className="bg-primary">
+                    <div className="container" id="home">
+                        <div className="split">
+                            <div className="text">
                                 <hgroup>
                                     <h2>#2 TOP BEST DEV PLATAFORM</h2>
                                     <h1>Experiência alinhada ao conhecimento!</h1>
                                 </hgroup>
                                 <p>Anuncie seus trabalhos e encontre clientes de forma simples e rápida.</p>
-                                <div class="button-text">
-                                    <a class="btn btn-imdev" href="#" role="button">Sou Desenvolvedor</a>
-                                    <a class="btn btn-searchdev" href="#" role="button">Busco Desenvolvedores</a>
+                                <div className="button-text">
+                                    <a className="btn btn-imdev" href="#" role="button">Sou Desenvolvedor</a>
+                                    <a className="btn btn-searchdev" href="#" role="button">Busco Desenvolvedores</a>
                                 </div>
                             </div>
 
-                            <div class="image">
-                                <img src="./img/pag1.svg" class="img-reponsive img-boydesktop" alt="happy boy in desktop" />
-                                <img src="img/bottom-mobile.svg" class="bottom-image" />
+                            <div className="image">
+                                <img src={imgPage1} className="img-reponsive img-boydesktop" alt="happy boy in desktop" />
+                                <img src="img/bottom-mobile.svg" className="bottom-image" />
                             </div>
                         </div>
                     </div>
                 </section>
 
-                <section class="bg-accent">
-                    <div class="container" id="devtask">
-                        <div class="split" >
-                            <div class="text">
+                <section className="bg-accent">
+                    <div className="container" id="devtask">
+                        <div className="split" >
+                            <div className="text">
                                 <hgroup>
                                     <h2>#2 TOP BEST DEV PLATAFORM</h2>
                                     <h1>O DevTask</h1>
                                 </hgroup>
                                 <p>A DevTask é a plataforma que conecta os desenvolvedores em começo de carreira com novas oportunidades, para dar aquele match com os clientes e um up na sua experiência.</p>
-                                <div class="button-text">
-                                    <a class="btn btn-github" href="https://github.com/ICEI-PUC-Minas-PPLES-TI/DevTask" target="_blank">Repositório no GitHub</a>
+                                <div className="button-text">
+                                    <a className="btn btn-github" href="https://github.com/ICEI-PUC-Minas-PPLES-TI/DevTask" target="_blank">Repositório no GitHub</a>
                                 </div>
                             </div>
-                            <div class="image">
-                                <img src="img/pag2.svg" class="img-reponsive img-boytablet" alt="boy using tablet" />
-                                <img src="./img/bottom-mobile.svg" class="bottom-image" />
+                            <div className="image">
+                                <img src={imgPage2} className="img-reponsive img-boytablet" alt="boy using tablet" />
+                                <img src="./img/bottom-mobile.svg" className="bottom-image" />
                             </div>
                         </div>
                     </div>
                 </section>
 
-                <section class="bg-basic">
-                    <div class="container" id="aboutUs">
-                        <div class="split">
-                            <div class="text">
+                <section className="bg-basic">
+                    <div className="container" id="aboutUs">
+                        <div className="split">
+                            <div className="text">
                                 <hgroup>
                                     <h2>TRABALHO INTERDISCIPLINAR - ICEI PUC MINAS</h2>
                                     <h1>Sobre Nós</h1>
                                 </hgroup>
                                 <p>Somos uma das melhores plataformas de desenvolvedores e trabalhamos para você desenvolver seus talentos e autonomia no trabalho em qualquer lugar do mundo.</p>
-                                <div class="button-text">
-                                    <a class="btn btn-github" href="https://github.com/ICEI-PUC-Minas-PPLES-TI/DevTask" target="_blank">Repositório no GitHub</a>
+                                <div className="button-text">
+                                    <a className="btn btn-github" href="https://github.com/ICEI-PUC-Minas-PPLES-TI/DevTask" target="_blank">Repositório no GitHub</a>
                                 </div>
                             </div>
-                            <div class="image">
-                                <img src="img/pag3.svg" class="img-reponsive" alt="DevTask team" />
+                            <div className="image">
+                                <img src={imgPage3} className="img-reponsive" alt="DevTask team" />
                             </div>
                         </div>
                     </div>
                 </section>
-                <script src="https://kit.fontawesome.com/634629ce87.js" crossorigin="anonymous"></script>
+                <script src="https://kit.fontawesome.com/634629ce87.js" crossOrigin="anonymous"></script>
             </>
         )
     }
