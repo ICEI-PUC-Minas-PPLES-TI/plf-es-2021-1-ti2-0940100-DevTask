a06608b9b7312b9bd6e334ab67fe586166ead165
Marlene Moraes <marlenevmoraes@outlook.com>
2021-05-10 23:14:06 -0300

    FRONT: refactor landing page #20

--- codigo/frontend/src/pages/landing-page/LandingPageModule.tsx
+++ codigo/frontend/src/pages/landing-page/LandingPageModule.tsx
@@ -9,76 +9,83 @@ export default class LandingPageModule extends Component {
     render() {
         return (
             <>
-                <div className="LandingPageModule1">
-                    <header>
-                        <div className="menu">
-                            <img src={logo} alt="DevTask logo" />
-                            <img className="menu-phone" src="img/bars.svg" alt="menu-hamburguer" />
-                            <nav className="navegacao">
-                                <ul>
-                                    <a href="#"><li>O DevTask</li></a>
-                                    <a href="#"><li>Sobre Nós</li></a>
-                                    <a href="#"><li><button className="cadastro">Cadastre-se</button></li></a>
-                                    <a href="#"><li><button className="login">Login</button></li></a>
-                                </ul>
-                            </nav>
-                        </div>
-                    </header>
+                <header class="header">
+                    <a class="logo" href="#home"><img src="./img/logo-white.svg" alt="DevTask logo" /></a>
+                    <a class="btn-menu"><i class="fas fa-bars"></i></a>
+                    <nav class="menu">
+                        <ul class="nav-list">
+                            <li><a class="nav-link" href="#devtask">O DevTask</a></li>
+                            <li><a class="nav-link" href="#aboutUs">Sobre Nós</a></li>
+                            <li><a class="nav-link btn btn-register" href="#" role="button">Cadastro</a></li>
+                            <li><a class="nav-link btn btn-login" href="#">Login</a></li>
+                        </ul>
+                    </nav>
+                </header>
 
-                    <div className="page1">
-                        <div className="text">
-                            <hgroup>
-                                <h2>#2 TOP BEST DEV PLATAFORM</h2>
-                                <h1>Experiência alinhada ao conhecimento!</h1>
-                            </hgroup>
-                            <p>Anuncie seus trabalhos e encontre clientes de forma simples e rápida.</p>
-                            <div className="register-anchors">
-                                <a id="sou-dev" href="/register?type=sou-dev">Sou Desenvolvedor</a>
-                                <a id="busco-dev" href="/register?type=busco-dev">Busco Desenvolvedores</a>
+                <section class="bg-primary">
+                    <div class="container" id="home">
+                        <div class="split">
+                            <div class="text">
+                                <hgroup>
+                                    <h2>#2 TOP BEST DEV PLATAFORM</h2>
+                                    <h1>Experiência alinhada ao conhecimento!</h1>
+                                </hgroup>
+                                <p>Anuncie seus trabalhos e encontre clientes de forma simples e rápida.</p>
+                                <div class="button-text">
+                                    <a class="btn btn-imdev" href="#" role="button">Sou Desenvolvedor</a>
+                                    <a class="btn btn-searchdev" href="#" role="button">Busco Desenvolvedores</a>
+                                </div>
                             </div>
-                        </div>
-                        <div className="image">
-                            <img src={imgPage1} alt="Desenvolvedor" />
-                            <img className="bottom-image" src="img/bottom-mobile.svg" />
-                        </div>
-                    </div>
-                </div>
-                <div className="LandingPageModule2">
-                    <div className="page2">
-                        <div className="text">
-                            <hgroup>
-                                <h2>#2 TOP BEST DEV PLATAFORM</h2>
-                                <h1>O DevTask</h1>
-                            </hgroup>
-                            <p>A DevTask é a plataforma que conecta os desenvolvedores em começo de carreira com novas oportunidades, para dar aquele match com os clientes e um up na sua experiência.</p>
-                            <div className="register-anchors">
-                                <a id="github" href="https://github.com/ICEI-PUC-Minas-PPLES-TI/DevTask" target="_blank">Repositório no GitHub</a>
-                            </div>
-                        </div>
-                        <div className="image">
-                            <img src={imgPage2} alt="Desenvolvedor2" />
-                            <img className="bottom-image" src="img/bottom-mobile.svg" />
-                        </div>
-                    </div>
-                </div>
 
-                <div className="LandingPageModule3">
-                    <div className="page3">
-                        <div className="text">
-                            <hgroup>
-                                <h2>TRABALHO INTERDISCIPLINAR - ICEI PUC MINAS</h2>
-                                <h1>Sobre Nós</h1>
-                            </hgroup>
-                            <p>Somos uma das melhores plataformas de desenvolvedores e trabalhamos para você desenvolver seus talentos e autonomia no trabalho em qualquer lugar do mundo.</p>
-                            <div className="register-anchors">
-                                <a id="github" href="https://github.com/ICEI-PUC-Minas-PPLES-TI/DevTask" target="_blank">Repositório no GitHub</a>
+                            <div class="image">
+                                <img src="./img/pag1.svg" class="img-reponsive img-boydesktop" alt="happy boy in desktop" />
+                                <img src="img/bottom-mobile.svg" class="bottom-image" />
                             </div>
                         </div>
-                        <div className="image">
-                            <img src={imgPage3} alt="Equipe DevTask" />
+                    </div>
+                </section>
+
+                <section class="bg-accent">
+                    <div class="container" id="devtask">
+                        <div class="split" >
+                            <div class="text">
+                                <hgroup>
+                                    <h2>#2 TOP BEST DEV PLATAFORM</h2>
+                                    <h1>O DevTask</h1>
+                                </hgroup>
+                                <p>A DevTask é a plataforma que conecta os desenvolvedores em começo de carreira com novas oportunidades, para dar aquele match com os clientes e um up na sua experiência.</p>
+                                <div class="button-text">
+                                    <a class="btn btn-github" href="https://github.com/ICEI-PUC-Minas-PPLES-TI/DevTask" target="_blank">Repositório no GitHub</a>
+                                </div>
+                            </div>
+                            <div class="image">
+                                <img src="img/pag2.svg" class="img-reponsive img-boytablet" alt="boy using tablet" />
+                                <img src="./img/bottom-mobile.svg" class="bottom-image" />
+                            </div>
                         </div>
                     </div>
-                </div>
+                </section>
+
+                <section class="bg-basic">
+                    <div class="container" id="aboutUs">
+                        <div class="split">
+                            <div class="text">
+                                <hgroup>
+                                    <h2>TRABALHO INTERDISCIPLINAR - ICEI PUC MINAS</h2>
+                                    <h1>Sobre Nós</h1>
+                                </hgroup>
+                                <p>Somos uma das melhores plataformas de desenvolvedores e trabalhamos para você desenvolver seus talentos e autonomia no trabalho em qualquer lugar do mundo.</p>
+                                <div class="button-text">
+                                    <a class="btn btn-github" href="https://github.com/ICEI-PUC-Minas-PPLES-TI/DevTask" target="_blank">Repositório no GitHub</a>
+                                </div>
+                            </div>
+                            <div class="image">
+                                <img src="img/pag3.svg" class="img-reponsive" alt="DevTask team" />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <script src="https://kit.fontawesome.com/634629ce87.js" crossorigin="anonymous"></script>
             </>
         )
     }
