b930a8b285036e5dd09c855d82aadfcff586eef9
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2021-05-14 01:09:17 -0300

    FRONT: Update Login Modal #47

--- codigo/frontend/src/components/login-modal/LoginModal.tsx
+++ codigo/frontend/src/components/login-modal/LoginModal.tsx
@@ -1,53 +1,100 @@
 import { Grid, InputAdornment, Link, TextField } from '@material-ui/core'
 import { Email, LockOpen } from '@material-ui/icons'
-import { Component } from 'react'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import * as yup from 'yup'
+import React, { Component } from 'react'
+import login from '../../services/login'
 import './LoginModal.css'
 
-class LoginModal extends Component {
+export default class LoginModal extends Component {
+
+    data = {
+        email: document.getElementById('email')?.nodeValue,
+        password: document.getElementById('password')?.nodeValue
+    }
+
+    handleChange = (event: { target: { id: any; value: any } }) => {
+        switch (event.target.id) {
+            case 'email':
+                this.data.email = event.target.value
+                break;
+            case 'password':
+                this.data.password = event.target.value;
+                break;
+        }
+    }
+
+    validations = yup.object().shape({
+        email: yup.string().email().required(),
+        password: yup.string().min(8).required()
+    })
+
+    handleSubmit = (values: any) => {
+        console.log(values);
+    }
+
     render() {
         return (
             <Grid container className="LoginModal">
                 <Grid item xs={12}>
-                <form autoComplete="off">
-                    <Grid item xs={12}>
-                        <h2>Login</h2>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            fullWidth
-                            margin={"normal"}
-                            label="Email"
-                            variant="outlined"
-                            type="mail"
-                            required
-                            InputProps={{
-                                startAdornment: (
-                                  <InputAdornment position="start">
-                                    <Email />
-                                  </InputAdornment>
-                                ),
-                              }}/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            fullWidth
-                            margin={"normal"}
-                            label="Senha"
-                            variant="outlined"
-                            type="password"
-                            required
-                            InputProps={{
-                                startAdornment: (
-                                  <InputAdornment position="start">
-                                    <LockOpen />
-                                  </InputAdornment>
-                                ),
-                              }}/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <button type="submit">Entrar</button>
-                    </Grid>
-                </form>
+                    <Formik
+                        initialValues={{}}
+                        onSubmit={this.handleSubmit}
+                        validationSchema={this.validations}
+                    >
+                        <Form method="post">
+                            <Grid item xs={12}>
+                                <h2>Login</h2>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Field
+                                    name="email"
+                                    fullWidth
+                                    margin={"normal"}
+                                    label="Email"
+                                    variant="outlined"
+                                    type="mail"
+                                    onChange={this.handleChange}
+                                    required
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <Email />
+                                            </InputAdornment>
+                                        ),
+                                    }} />
+                                <ErrorMessage
+                                    component="span"
+                                    name="email"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Field
+                                    name="password"
+                                    fullWidth
+                                    margin={"normal"}
+                                    label="Senha"
+                                    variant="outlined"
+                                    type="password"
+                                    onChange={this.handleChange}
+                                    required
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <LockOpen />
+                                            </InputAdornment>
+                                        ),
+                                    }} />
+                                <ErrorMessage
+                                    component="span"
+                                    name="password"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <button type="submit">Entrar</button>
+                            </Grid>
+                        </Form>
+                    </Formik>
                 </Grid>
                 <Grid item xs={12}>
                     <p>Ainda não possui uma conta? <p className="link"><Link href="/register" color="inherit">Cadastre-se</Link></p></p>
@@ -56,5 +103,3 @@ class LoginModal extends Component {
         )
     }
 }
-
-export default LoginModal
